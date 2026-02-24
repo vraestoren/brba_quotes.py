1c4e8773ec4f87ab7a543b79072f0d9751995be8
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 15:29:21 +0300

    Update brba_quotes.py

--- src/brba_quotes.py
+++ src/brba_quotes.py
@@ -1,18 +1,15 @@
-from requests import get
+from requests import Session
 
 class BrBaQuotes:
 	def __init__(self) -> None:
 		self.api = "https://api.breakingbadquotes.xyz"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
 	def get_random_quote(self) -> dict:
-		return get(
-			f"{self.api}/v1/quotes"
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/v1/quotes").json()
 
 	def get_multiple_quotes(self, count: int) -> dict:
-		return get(
-			f"{self.api}/v1/quotes/{count}"
-			headers=self.headers).json()	
+		return self.session.get(f"{self.api}/v1/quotes/{count}").json()	
